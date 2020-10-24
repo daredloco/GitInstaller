416591b6db49e39ad4e4b3b72bf10794d10057c4
daredloco <deadlybeatz@hotmail.com>
2020-10-24 09:33:15 -0300

    Added Utils class
    Added Wildcard to the Utils class to return a list of elements when entering a list of strings, a wildcard (eg. test*) and a wildcardchar (eg. *).
    Wildcardchar is optional, can be any string

---
+++ GitInstaller/GitInstaller/Utils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitInstaller
+{
+	public static class Utils
+	{
+		public static List<string> Wildcard(string[] list, string wildcard, string wildcardchar = "*")
+		{
+			List<string> lst = new List<string>();
+			bool startswith = false;
+			if(wildcard.EndsWith(wildcardchar))
+			{
+				startswith = false;
+				wildcard = wildcard.Remove(wildcard.Length - 1);
+			}
+			else if(wildcard.StartsWith(wildcardchar))
+			{
+				startswith = true;
+				wildcard = wildcard.Remove(0, 1);
+			}else
+			{
+				throw new Exception("Invalid wildcard (Use wildcard* or *wildcard)");
+			}
+
+			foreach(string element in list)
+			{
+				if(startswith)
+				{
+					if (element.StartsWith(wildcard))
+						lst.Add(element);
+				}
+				else
+				{
+					if (element.EndsWith(wildcard))
+						lst.Add(element);
+				}
+			}
+			return lst;
+		}
+	}
+}
